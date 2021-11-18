a09eb24899f6f7119f02fa310d3f822414309557
disbr007 <disbr007@umn.edu>
2021-11-18 15:10:02 -0500

    correct user variable

--- cicd/create-release-notes.sh
+++ cicd/create-release-notes.sh
@@ -3,4 +3,4 @@ VERSION=${1/v}
 USER=${2}
 echo "Version: ${VERSION}"
 echo "Released version=${VERSION}" > RELEASE_NOTES.md
-echo "Released by=${USER}" > RELEASE_NOTES.md
+echo "Released by=${USER}" >> RELEASE_NOTES.md
